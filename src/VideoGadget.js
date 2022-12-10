9def7231fced43d29c65ba64acd1ec0092b45913
Benjamin Smith <bsmith@cabbage.org.uk>
2022-12-10 16:45:43 +0000

    Chroma Keying experiment

--- src/VideoGadget.js
+++ src/VideoGadget.js
@@ -1,7 +1,55 @@
 import { useEffect, useRef, useState } from 'react';
 import './VideoGadget.scss';
 
-/* for a nice Van Gogh effect I had gamma at 0.5, and the boost factor at 100.
+const getElementPosition = (elem) => {
+    var curleft = 0, curtop = 0;
+    if (elem.offsetParent) {
+        do {
+            curleft += elem.offsetLeft;
+            curtop += elem.offsetTop;
+            elem = elem.offsetParent;
+        } while (elem);
+        return [curleft, curtop];
+    }
+    return undefined;
+}
+
+/* for a nice Van Gogh effect I had gamma at 0.5, and the boost factor at 100. */
+
+/* takes a 0-255 sRGB and outputs 0-1 float */
+const srgbToLinear = (srgb) => {
+    const gamma = ((srgb/255 + 0.055) / 1.055)**2.4;
+    const scale = srgb/255 / 12.92;
+    return srgb/255 > 0.04045 ? gamma : scale;
+};
+
+/* takes a 0-1 float and outputs a 0-255 sRGB int */
+const linearToSrgb = (lin) => {
+    const gamma = 1.055 * (lin ** (1/2.4)) - 0.055;
+    const scale = lin * 12.92;
+    const srgb = lin > 0.0031308 ? gamma : scale;
+    return Math.round(255 * srgb);
+}
+
+/* 0.18 is equiv to 118 */
+console.log("0.18", srgbToLinear(118));
+console.log("118", linearToSrgb(0.18));
+
+const calcChromaKey = (image) => {
+    const {width, height, data} = image;
+    let [r, g, b] = [0, 0, 0];
+    for (let x = 0; x < width; x++)
+        for (let y = 0; y < height; y++) {
+            const index = y * width * 4 + x * 4;
+            r += srgbToLinear(data[index+0]);
+            g += srgbToLinear(data[index+1]);
+            b += srgbToLinear(data[index+2]);
+        }
+    r = linearToSrgb(r / (width*height));
+    g = linearToSrgb(g / (width*height));
+    b = linearToSrgb(b / (width*height));
+    return [r, g, b];
+};
 
 /* assume a heart rate between 60 and 120 bpm;
    this is 1 to 2 s^-1
@@ -9,7 +57,7 @@ import './VideoGadget.scss';
    5 to 20 fps target range */
 
 /* accumData could be any Uint8ClampedArray, and only store the 1 channel */
-const mungeImage = (image, outputData, accumData) => {
+const mungeImage = (image, outputData, accumData, chromaKey) => {
     const imageData = image.data;
     const {width, height} = image;
     let signal = 0;
@@ -17,22 +65,40 @@ const mungeImage = (image, outputData, accumData) => {
     for (let x = 0; x < width; x++)
         for (let y = 0; y < height; y++) {
             const index = y * width * 4 + x * 4;
+            const input = [imageData[index+0], imageData[index+1], imageData[index+2]];
+
+            let output = [0, 0, 0];
+            let alpha = 0;
+
+            const chromaDist = Math.sqrt((input[0] - chromaKey[0]) ** 2 + (input[1] - chromaKey[1]) ** 2 + (input[2] - chromaKey[2]) ** 2);
+            if (chromaDist < 50) {
+                alpha = 1;
+                output = [0, 0, 255 - chromaDist];
+            }
+
+            /* chroma keying stuff */
+            /* NB alpha-blend in the WRONG SPACE XXX */
+            outputData[index+0] = (1-alpha)*imageData[index+0] + alpha * output[0]; /* R */
+            outputData[index+1] = (1-alpha)*imageData[index+1] + alpha * output[1]; /* G */
+            outputData[index+2] = (1-alpha)*imageData[index+2] + alpha * output[2]; /* B */
+            outputData[index+3] = 255; /* A */
 
             /* the kernel */
-            const gamma = 0.5;
-            let accum = accumData[index+1];
-            const input = imageData[index+1];
-            accum = (1-gamma)*accum + gamma*input;
-            const output = input + (accum - input) * 100;
+            // const gamma = 0.5;
+            // let accum = accumData[index+1];
+            // const input = imageData[index+1];
+            // accum = (1-gamma)*accum + gamma*input;
+            // const output = input + (accum - input) * 100;
 
-            signal += Math.abs(accum - input);
+            // signal += Math.abs(accum - input);
 
-            accumData[index+1] = accum; /* write back */
+            // accumData[index+1] = accum; /* write back */
 
-            outputData[index+0] = 0; /* R */
-            outputData[index+1] = output; /* G */
-            outputData[index+2] = 0; /* B */
-            outputData[index+3] = 255; /* A */
+            // const alpha = 0.5;
+            // outputData[index+0] = alpha * 0; /* R */
+            // outputData[index+1] = alpha * output; /* G */
+            // outputData[index+2] = alpha * 0; /* B */
+            // outputData[index+3] = alpha * 255; /* A */
         }
     return { signal };
 };
@@ -45,9 +111,12 @@ const VideoGadget = () => {
         lastMillis: Date.now(),
         intervalId: null,
         buffer: [], /*Array(12 * 10).fill(0)*/
+        clickLocation: null,
+        chromaKey: null,
     });
     const [fpsCounter, setFpsCounter] = useState(0);
     const [outputDisplay, setOutputDisplay] = useState("");
+    const [chromaColour, setChromaColour] = useState('#000000');
 
     const startVideo = async () => {
         const media = await navigator.mediaDevices.getUserMedia({
@@ -74,6 +143,14 @@ const VideoGadget = () => {
         canvasRef.current.height = 480;
         context.drawImage(videoRef.current, 0, 0, 640, 480);
 
+        let { clickLocation, chromaKey } = stateRef.current;
+        if (!chromaKey && clickLocation) {
+            const imageData = context.getImageData(clickLocation[0] - 2, clickLocation[1] - 2, 5, 5);
+            chromaKey = stateRef.current.chromaKey = calcChromaKey(imageData);
+            console.log("chromaKey", chromaKey);
+            setChromaColour(`rgb(${chromaKey[0]}, ${chromaKey[1]}, ${chromaKey[2]})`);
+        }
+
         const centreAreaSize = 200;
         const centreAreaPos = [(640 - centreAreaSize) / 2, (480 - centreAreaSize) / 2];
         const imageData = context.getImageData(centreAreaPos[0], centreAreaPos[1], centreAreaSize, centreAreaSize);
@@ -90,16 +167,20 @@ const VideoGadget = () => {
             stateRef.current.accumData.data.fill(127);
         }
 
-        const processedData = context.createImageData(imageData);
-        const { signal } = mungeImage(imageData, processedData.data, stateRef.current.accumData.data);
-        // console.log(processedData);
-        // console.log(signal);
-        const { buffer } = stateRef.current;
-        // buffer.shift();
-        buffer.push(signal);
-        // console.log(buffer);
+        if (chromaKey) {
+            const processedData = context.createImageData(imageData);
+            const { signal } = mungeImage(imageData, processedData.data, stateRef.current.accumData.data, chromaKey);
+            // const { buffer } = stateRef.current;
+            // buffer.push(signal);
 
-        context.putImageData(processedData, centreAreaPos[0], centreAreaPos[1]);
+            context.putImageData(processedData, centreAreaPos[0], centreAreaPos[1]);
+        }
+
+        if (clickLocation) {
+            context.beginPath();
+            context.ellipse(clickLocation[0], clickLocation[1], 5, 5, 0, 0, 2*Math.PI);
+            context.stroke();
+        }
     };
 
     const stopCaptures = () => {
@@ -125,17 +206,26 @@ const VideoGadget = () => {
         if (stopCaptures())
             return;
         stateRef.current.intervalId = setInterval(captureImage, 1000 / targetFps);
-        setTimeout(() => {
+        if (0) setTimeout(() => {
             stopCaptures();
         }, 10000)
+    };
+
+    const canvasClick = (event) => {
+        const pos = getElementPosition(event.target);
+        stateRef.current.clickLocation = [event.pageX - pos[0], event.pageY - pos[1]];
+        stateRef.current.chromaKey = null;
+        // console.log(event);
+        // console.log(stateRef.current.clickLocation);
     };
 
     return <div className="VideoGadget">
         <div className="video">
             <video ref={videoRef} autoPlay controls />
-            <canvas ref={canvasRef} />
+            <canvas ref={canvasRef} onClick={canvasClick} />
         </div>
         <output className="fps-counter">{fpsCounter.toFixed(4)}</output>
+        <output className="chroma-colour" style={{backgroundColor: chromaColour}}>{chromaColour}</output>
         <div className="controls">
             <button onClick={startVideo}>Start Video</button>
             <button onClick={stopVideo}>Stop Video</button>
